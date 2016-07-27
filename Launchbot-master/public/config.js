29bfa08a7437acbfd46515a3e9cd794a8d4a327d
ben frank <ben frank>
2016-07-27 11:17:55 -0400

    Changed Layouts

---
+++ Launchbot-master/public/config.js
@@ -0,0 +1,88 @@
+var launchbotConfig = {
+    // Enter absolute URIs 'http://', 'https://', 'ftp://' …
+    sets: [
+        // First example set
+        {
+            name: 'Daily',
+            items: [
+                'http://learn.digitalharbor.org',
+                'http://youth.digitalharbor.org',
+                'https://mail.google.com',
+                'https://drive.google.com',
+                'http://www.fabhive.buzz/megalab/'
+            ],
+        },
+        // Second example set
+        {
+            name: 'Members',
+            items: [
+                'http://michaelxander.com/static/nyc-cams.html',
+                'http://skyscanner.de', 
+                'http://kayak.com',
+                'http://en.wikivoyage.org', 
+                'http://wikitravel.org'
+            ]
+        }
+        // Add more sets below, don’t forget to add a comma after the previous set.
+    ],
+    // For favorites bar, enter absolute URIs 'http://', 'https://', 'ftp://' …
+    favs: [
+        'http://digitalharbor.org', 
+        'http://youth.digitalharbor.org/',        
+        'https://mail.google.com', 
+        'https://drive.google.com',
+        'https://app.asana.com', 
+        'https://twitter.com',
+        'https://hootsuite.com', 
+        'http://nytimes.com',
+        'https://news.ycombinator.com', 
+        'http://onethingwell.org',
+        'http://brettterpstra.com'
+    ],
+   /* images: [
+        'images/DHF-Logo-Learn.png', 
+        'images/DHF-Logo.png',
+        'images/New_Logo_Gmail.png', 
+        'images/Logo_of_Google_Drive.png',
+        'images/asana.png', 
+        'images/twitter-128.png',
+        'images/hootsuite-icon-16295.png',
+        'images/new-york-times-icon.png', 
+        'images/hackernews.png',
+        'images/otw.png', 
+        'images/bp.jpg' 
+    ],*/
+    
+    // Optional settings
+    options: {
+        searchEngine: 'https://www.google.com/search',
+        faviconSize: 16,
+        faviconService: 'http://www.google.com/s2/favicons?domain=',
+        linkTarget: '_blank'
+    },
+    
+    //console.log("hello world");
+    
+    plugins: {
+        weather: {
+            enabled: true,
+            zipCode: 21230,
+            country: 'United States',
+            useCelcius: true
+        }
+    },
+    options2: {
+        size: 60,
+        //add image paths for each image icon
+        //the for loop in the getLinks function will select the image
+        //in order from 0,1,2,3,...
+        /*image: [
+        '/images/DHF-Logo.png',
+        '/images/DHF-Logo-Learn.png'    
+
+        
+    ],*/
+        image: ['images/DHF-Logo.png', 'images/DHF-Logo-Learn.png'],
+        linkTarget: '_blank'
+    }
+};
